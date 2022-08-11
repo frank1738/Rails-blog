12ebee11b7f67760c7a59d523867a6ae8e34c581
frank1738 <franklineosoro08@gmail.com>
2022-08-11 12:57:28 +0300

    fix linter errors

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,6 +2,7 @@ class Post < ApplicationRecord
   def self.update_posts_counter
     author.increment!(:posts_counter)
   end
+  
   def self.recent_comments
     Comment.order(created_at: :desc).limit(5)
   end
