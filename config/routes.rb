a4b596b0cc3203ac2735dc5c5c2ddcc65bc80e65
jmoseka <jamylam3@gmail.com>
2022-08-22 17:19:27 +0300

    Fix linters error

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@
 Rails.application.routes.draw do
   resources :posts
   root 'users#index'
-  resources :users, only: [:index,:show] do
+  resources :users, only: %i[index show] do
     resources :posts do
       resources :comments, only: %i[new create destroy]
       resources :likes, only: %i[create new]
