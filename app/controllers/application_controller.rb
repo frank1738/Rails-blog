9f5456c1cf2343815038920921fcba17a762616b
jmoseka <jamylam3@gmail.com>
2022-08-23 15:10:18 +0300

    Setup and install devise

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,2 @@
 class ApplicationController < ActionController::Base
-  def current_user
-    @current_u = User.first
-  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  devise_for :admins
   resources :posts
   root 'users#index'
   resources :users, only: %i[index show] do
