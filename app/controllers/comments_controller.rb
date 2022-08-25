cfa10a014dfa752ce94ee1092827d0c44a35d841
jmoseka <jamylam3@gmail.com>
2022-08-25 17:50:24 +0300

    Update file

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -19,16 +19,6 @@ class CommentsController < ApplicationController
     end
   end
 
-  def destroy
-    puts params
-    @post = Post.find(params[:post_id])
-    comment = @post.comment.find(params[:id])
-    if comment.destroy
-      redirect_to user_post_url
-    else
-      redirect_to user_posts_url
-    end
-  end
 
   private
 
